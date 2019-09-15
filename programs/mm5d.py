d57cd803f21956f76d1bcf5ab0d1eca18b788767
Pozsár Zsolt <pozsarzs@karak>
2019-09-15 20:59:42 +0200

    190915

---
+++ programs/mm5d.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# +----------------------------------------------------------------------------+
+# | MM5D v0.1 * Growing house controlling and remote monitoring system         |
+# | Copyright (C) 2019 Pozsar Zsolt <pozsar.zsolt@.szerafingomba.hu>           |
+# | mm5d.py                                                                    |
+# | Main program                                                               |
+# +----------------------------------------------------------------------------+
+#
+#   This program is free software: you can redistribute it and/or modify it
+# under the terms of the European Union Public License 1.1 version.
+#
+#   This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.
---
+++ programs/prg_example/cfg_example.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# +----------------------------------------------------------------------------+
+# | MM5D v0.1 * Growing house controlling and remote monitoring system         |
+# | Copyright (C) 2019 Pozsar Zsolt <pozsar.zsolt@.szerafingomba.hu>           |
+# | cfg_example.sh                                                             |
+# | User's program configurator                                                |
+# +----------------------------------------------------------------------------+
+#
+#   This program is free software: you can redistribute it and/or modify it
+# under the terms of the European Union Public License 1.1 version.
+#
+#   This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.
+

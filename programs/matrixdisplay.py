9167cc0eb19e64e7fa46a7d73853a5928adecb76
Pozsar Zsolt <pozsarzs@gmail.com>
2020-04-19 20:40:11 +0200

    200419

--- programs/matrixdisplay.py
+++ programs/matrixdisplay.py
@@ -27,16 +27,16 @@ from luma.led_matrix.device import max7219
 
 file_pipe="/var/tmp/matrixdisplayfifo"
 
-serial=spi(port=0, device=0, gpio=noop())
-device=max7219(serial, width=32, height=8, block_orientation=-90)
-device.contrast(5)
-virtual=viewport(device, width=32, height=8)
 s=''
 while True:
   try:
     with open(file_pipe, "r") as fifo:
       s=fifo.read()
       fifo.close()
+      serial=spi(port=0, device=0, gpio=noop())
+      device=max7219(serial, width=32, height=8, block_orientation=-90)
+      device.contrast(5)
+      virtual=viewport(device, width=32, height=8)
   except:
     print ("")
   with canvas(virtual) as draw:
